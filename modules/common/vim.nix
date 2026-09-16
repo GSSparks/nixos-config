6a840b6b7f9f0acbb672cf4606a288861e3eba9e
Gary Sparks <gsparks@sitespect.com>
2026-09-16 10:56:23 -0400

    fix: colorscheme in vim

--- modules/common/vim.nix
+++ modules/common/vim.nix
@@ -3,7 +3,7 @@
   environment.variables = { EDITOR = "vim"; };
 
   environment.systemPackages = with pkgs; [
-    ((vim_configurable.override {  }).customize{
+    ((vim-full.override {  }).customize{
       name = "vim";
       vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
         start = [ vim-nix vim-lastplace nerdtree vim-polyglot vim-gitgutter vim-fugitive YouCompleteMe supertab gruvbox-material vim-airline vim-airline-themes ];
@@ -71,13 +71,13 @@
         let g:airline_symbols.paste = 'Þ'
         let g:airline_symbols.paste = '∥'
         let g:airline_symbols.whitespace = 'Ξ'
-        let g:airline_left_sep = ''
-        let g:airline_left_alt_sep = ''
-        let g:airline_right_sep = ''
-        let g:airline_right_alt_sep = ''
-        let g:airline_symbols.branch = ''
-        let g:airline_symbols.readonly = ''
-        let g:airline_symbols.linenr = ''
+        let g:airline_left_sep = '''
+        let g:airline_left_alt_sep = '''
+        let g:airline_right_sep = '''
+        let g:airline_right_alt_sep = '''
+        let g:airline_symbols.branch = '''
+        let g:airline_symbols.readonly = '''
+        let g:airline_symbols.linenr = '''
       '';
     }
   )];
